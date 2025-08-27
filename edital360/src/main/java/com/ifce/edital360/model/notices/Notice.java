63b5bda81ecf4c6573714fdb9083c3263cef4a3a
juliano magalhaes <juliano.magalhaes06@aluno.ifce.edu.br>
2025-08-27 15:35:09 -0300

    feat: inicio da modelagem das entidades

--- edital360/src/main/java/com/ifce/edital360/model/notices/Notice.java
+++ edital360/src/main/java/com/ifce/edital360/model/notices/Notice.java
@@ -1,6 +1,7 @@
 package com.ifce.edital360.model.notices;
 
 
+import com.ifce.edital360.model.enums.Exam;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
@@ -16,17 +17,51 @@ public class Notice {
     private UUID id;
 
     private String title;
-    private String numVacancies;
+    private String description;
     private Float remuneration;
     private LocalDateTime initialDate;
     private LocalDateTime endDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime examDate;
+
 
     @ElementCollection
-    private List<String> roles;
+    @CollectionTable(name = "notice_phases", joinColumns = @JoinColumn(name = "notice_id"))
+    private List<Phase> phases;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "notice_roles",
+            joinColumns = @JoinColumn(name = "notice_id")
+    )
+    private List<NoticeRole> roles;
+
+    @ElementCollection
+    private List<String> requirements;
+
+    @ElementCollection
+    private List<String> documents;
 
     @OneToMany(mappedBy = "notice", cascade = CascadeType.ALL,  orphanRemoval = true)
     private List<ExternalLinks> externalLinks;
+
+    @OneToMany(mappedBy = "notice",  cascade = CascadeType.ALL,  orphanRemoval = true)
+    private List<Announcements> announcements;
+
+    @ElementCollection
+    private List<String> quotas;
+    private Float subscription;
     private String pdfUrl;
+
+    @ElementCollection
+    @CollectionTable(name = "notice_schedule", joinColumns = @JoinColumn(name = "notice_id"))
+    private List<ScheduleItem> schedule;
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 
     public UUID getId() {
         return id;
@@ -44,12 +79,12 @@ public class Notice {
         this.title = title;
     }
 
-    public String getNumVacancies() {
-        return numVacancies;
+    public String getDescription() {
+        return description;
     }
 
-    public void setNumVacancies(String numVacancies) {
-        this.numVacancies = numVacancies;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Float getRemuneration() {
@@ -76,12 +111,52 @@ public class Notice {
         this.endDate = endDate;
     }
 
-    public List<String> getRoles() {
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getExamDate() {
+        return examDate;
+    }
+
+    public void setExamDate(LocalDateTime examDate) {
+        this.examDate = examDate;
+    }
+
+    public List<Phase> getPhases() {
+        return phases;
+    }
+
+    public void setPhases(List<Phase> phases) {
+        this.phases = phases;
+    }
+
+    public List<NoticeRole> getRoles() {
         return roles;
     }
 
-    public void setRoles(List<String> roles) {
+    public void setRoles(List<NoticeRole> roles) {
         this.roles = roles;
+    }
+
+    public List<String> getRequirements() {
+        return requirements;
+    }
+
+    public void setRequirements(List<String> requirements) {
+        this.requirements = requirements;
+    }
+
+    public List<String> getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(List<String> documents) {
+        this.documents = documents;
     }
 
     public List<ExternalLinks> getExternalLinks() {
@@ -92,6 +167,30 @@ public class Notice {
         this.externalLinks = externalLinks;
     }
 
+    public List<Announcements> getAnnouncements() {
+        return announcements;
+    }
+
+    public void setAnnouncements(List<Announcements> announcements) {
+        this.announcements = announcements;
+    }
+
+    public List<String> getQuotas() {
+        return quotas;
+    }
+
+    public void setQuotas(List<String> quotas) {
+        this.quotas = quotas;
+    }
+
+    public Float getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(Float subscription) {
+        this.subscription = subscription;
+    }
+
     public String getPdfUrl() {
         return pdfUrl;
     }
@@ -100,15 +199,32 @@ public class Notice {
         this.pdfUrl = pdfUrl;
     }
 
-    public Notice(String title, Float remuneration, String numVacancies, LocalDateTime initialDate, LocalDateTime endDate, List<String> roles, List<ExternalLinks> externalLinks, String pdfUrl) {
+    public List<ScheduleItem> getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(List<ScheduleItem> schedule) {
+        this.schedule = schedule;
+    }
+
+    public Notice(String title, String description, Float remuneration, LocalDateTime initialDate, LocalDateTime endDate, LocalDateTime createdAt, LocalDateTime examDate, List<Phase> phases, List<NoticeRole> roles, List<String> requirements, List<String> documents, List<ExternalLinks> externalLinks, List<Announcements> announcements, List<String> quotas, Float subscription, String pdfUrl, List<ScheduleItem> schedule) {
         this.title = title;
+        this.description = description;
         this.remuneration = remuneration;
-        this.numVacancies = numVacancies;
         this.initialDate = initialDate;
         this.endDate = endDate;
+        this.createdAt = createdAt;
+        this.examDate = examDate;
+        this.phases = phases;
         this.roles = roles;
+        this.requirements = requirements;
+        this.documents = documents;
         this.externalLinks = externalLinks;
+        this.announcements = announcements;
+        this.quotas = quotas;
+        this.subscription = subscription;
         this.pdfUrl = pdfUrl;
+        this.schedule = schedule;
     }
 
     public Notice() {
