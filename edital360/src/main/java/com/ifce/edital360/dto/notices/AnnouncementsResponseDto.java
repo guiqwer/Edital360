d6412e20b2fdce0b22acd9f9d6c7579c5aa92d28
juliano magalhaes <juliano.magalhaes06@aluno.ifce.edu.br>
2025-09-08 16:47:24 -0300

    feat: rota de cadastro feita, falta corrigir erros

--- edital360/src/main/java/com/ifce/edital360/dto/notices/AnnouncementsResponseDto.java
+++
@@ -1,6 +0,0 @@
-package com.ifce.edital360.dto.notices;
-
-public record AnnouncementsResponseDto(
-        String file
-) {
-}
--- edital360/src/main/java/com/ifce/edital360/repository/ExternalLinksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ifce.edital360.repository;
-
-import com.ifce.edital360.model.notices.ExternalLinks;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ExternalLinksRepository extends JpaRepository<ExternalLinks, UUID> {
-}
